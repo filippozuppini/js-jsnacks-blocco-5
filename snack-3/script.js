917078c0cca691c53534342379bd359bc2d2f738
filippozuppini <zuppini.filippo@gmail.com>
2026-03-28 23:24:10 +0100

    snack03

--- snack-3/script.js
+++ snack-3/script.js
@@ -3,3 +3,6 @@ const numbers = [2, 8, 4, 7, 2, 87];
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
 
+
+
+numbers.forEach((number) => console.log(number+=1))
